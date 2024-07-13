8a3898e1ee612eb69f2574b86db0f2616090f619
ihorandev <ihorandev@gmail.com>
2024-07-13 09:57:11 +0200

    init

---
+++ constants/index.ts
@@ -0,0 +1,269 @@
+import { FaYoutube, FaFacebook } from "react-icons/fa";
+import {
+  RxDiscordLogo,
+  RxGithubLogo,
+  RxInstagramLogo,
+  RxTwitterLogo,
+  RxLinkedinLogo,
+} from "react-icons/rx";
+
+export const SOCIALS = [
+  {
+    name: "Instagram",
+    icon: RxInstagramLogo,
+    link: "https://www.instagram.com/igorandev/",
+  },
+  {
+    name: "Facebook",
+    icon: FaFacebook,
+    link: "https://www.facebook.com/GagarinGara/",
+  },
+  {
+    name: "Twitter",
+    icon: RxTwitterLogo,
+    link: "https://twitter.com",
+  },
+] as const;
+
+export const FRONTEND_SKILL = [
+  {
+    skill_name: "HTML",
+    image: "html.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    skill_name: "CSS",
+    image: "css.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    skill_name: "JavaScript",
+    image: "js.png",
+    width: 65,
+    height: 65,
+  },
+  {
+    skill_name: "Tailwind CSS",
+    image: "tailwind.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    skill_name: "React",
+    image: "react.png",
+    width: 80,
+    height: 80,
+  },
+
+  {
+    skill_name: "TypeScript",
+    image: "ts.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    skill_name: "Next.js 14",
+    image: "next.png",
+    width: 80,
+    height: 80,
+  },
+] as const;
+
+export const BACKEND_SKILL = [
+  {
+    skill_name: "Java",
+    image: "java.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    skill_name: "Spring Boot",
+    image: "springboot.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    skill_name: "Node.js",
+    image: "node.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    skill_name: "PostgreSQL",
+    image: "postgresql.png",
+    width: 70,
+    height: 70,
+  },
+  {
+    skill_name: "MongoDB",
+    image: "mongodb.png",
+    width: 40,
+    height: 40,
+  },
+  {
+    skill_name: "Firebase",
+    image: "firebase.png",
+    width: 55,
+    height: 55,
+  },
+  {
+    skill_name: "MySQL",
+    image: "mysql.png",
+    width: 70,
+    height: 70,
+  },
+  {
+    skill_name: "AWS",
+    image: "aws.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    skill_name: "Docker",
+    image: "docker.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    skill_name: "Kubernetes",
+    image: "kubernetes.png",
+    width: 80,
+    height: 80,
+  },
+] as const;
+
+export const OTHER_SKILL = [
+  {
+    skill_name: "Redux",
+    image: "redux.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    skill_name: "React Query",
+    image: "reactquery.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    skill_name: "Prisma",
+    image: "prisma.png",
+    width: 70,
+    height: 70,
+  },
+  {
+    skill_name: "Graphql",
+    image: "graphql.png",
+    width: 80,
+    height: 80,
+  },
+] as const;
+
+export const PROJECTS = [
+  {
+    title: "DevEducation Platform",
+    description:
+      'Embark on a journey through my professional evolution with the "DevEducation" - a dynamic showcase of my skills, experiences, and passion for web development. Crafted with precision and powered by the latest technologies, this portfolio is more than just a static display; it\'s an immersive experience that reflects the cutting edge of modern web technologies.',
+    image: "/projects/project-1.png",
+    link: "https://deveducation.com/en/",
+  },
+  {
+    title: "Metaminder",
+    description:
+      'Step into the extraordinary world of my professional journey through the "Metaminder Education Platform" - a platform where course and unique content creators can post their learning materials and students can take these courses with a great degree of customization of lessons and learning process. Artificial intelligence is installed in the role of the project guide. On this project I realized tasks as: frontend, backend and devops engineer.',
+    image: "/projects/project-2.png",
+    link: "https://platform.metaminder.com/",
+  },
+  {
+    title: "Space Themed Website",
+    description:
+      "With this next project, I'm going to open up a whole new approach to online education for you. This product is designed to educate people online, with artificial intelligence acting as a mentor. Students can hand in their homework and the teacher can check it and give advice on how to correct or improve it." +
+      "This project is under development with Java, Spring Boot as the backend and TypeScript, Next JS - 14 as the frontend. ",
+    image: "/projects/project-3.png",
+    link: "https://github.com/IhorAnDev/eduMentor",
+  },
+] as const;
+
+export const FOOTER_DATA = [
+  {
+    title: "Community",
+    data: [
+      {
+        name: "YouTube",
+        icon: FaYoutube,
+        link: "https://youtube.com",
+      },
+      {
+        name: "GitHub",
+        icon: RxGithubLogo,
+        link: "https://github.com/IhorAnDev",
+      },
+      {
+        name: "Discord",
+        icon: RxDiscordLogo,
+        link: "https://discord.com",
+      },
+    ],
+  },
+  {
+    title: "Social Media",
+    data: [
+      {
+        name: "Instagram",
+        icon: RxInstagramLogo,
+        link: "https://www.instagram.com/igorandev/",
+      },
+      {
+        name: "Twitter",
+        icon: RxTwitterLogo,
+        link: "https://twitter.com",
+      },
+      {
+        name: "Linkedin",
+        icon: RxLinkedinLogo,
+        link: "https://www.linkedin.com/in/developpapa/",
+      },
+    ],
+  },
+  {
+    title: "About",
+    data: [
+      {
+        name: "Become Sponsor",
+        icon: null,
+        link: "https://youtu.be/qTL0OMefKiE",
+      },
+      {
+        name: "Learning about me",
+        icon: null,
+        link: "https://example.com",
+      },
+      {
+        name: "Contact Me",
+        icon: null,
+        link: "mailto:ihorandevde@gmail.com",
+      },
+    ],
+  },
+] as const;
+
+export const NAV_LINKS = [
+  {
+    title: "About me",
+    link: "#about-me",
+  },
+  {
+    title: "Skills",
+    link: "#skills",
+  },
+  {
+    title: "Projects",
+    link: "#projects",
+  },
+] as const;
+
+export const LINKS = {
+  sourceCode: "",
+};
